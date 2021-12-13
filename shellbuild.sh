b84e1eb5bd1b8330df37092fc9d7daab013d6ff5
xiaoqiang <760925283@qq.com>
2021-12-13 16:12:24 +0800

    change code sign

--- shellbuild.sh
+++ shellbuild.sh
@@ -1,4 +1,8 @@
-xcodebuild archive -project SRCBTest.xcodeproj -scheme SRCBTest CODE_SIGN_IDENTITY="iPhone Distribution: SHANGHAI RURAL COMMERCIAL BANK CO., LTD." PROVISIONING_PROFILE="71ae8e52-ccc4-4142-ae94-d70ab2f1014f.mobileprovision"  -archivePath "./SRCBTest"
+codeIdentify="iPhone Distribution: Bank of Qingdao Co., Ltd."
+provisioningFile="76863b2c-73c3-40f9-97a0-d328f2d8872b.mobileprovision"
+#"iPhone Distribution: SHANGHAI RURAL COMMERCIAL BANK CO., LTD."
+#PROVISIONING_PROFILE="71ae8e52-ccc4-4142-ae94-d70ab2f1014f.mobileprovision"
+xcodebuild archive -project SRCBTest.xcodeproj -scheme SRCBTest CODE_SIGN_IDENTITY=$codeIdentify PROVISIONING_PROFILE=$provisioningFile  -archivePath "./SRCBTest"
 exportPath="./output/export"
 exportOptionPlist="./output/exportOptions.plist"
 xcodebuild -exportArchive -archivePath SRCBTest.xcarchive -exportPath $exportPath -exportOptionsPlist $exportOptionPlist
